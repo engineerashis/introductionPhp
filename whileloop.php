6b3e14c120605d59281ee273d008eca453dcf486
developerashisroy <ashisroy40089@gmail.com>
2022-01-26 02:09:08 +0530

    last git push comment

---
+++ whileloop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>while loop</title>
+</head>
+<body>
+    <?php
+    // loop : while, do while , for , for each; 
+    //  $a=1;
+    //  while($a<=10){
+    //      echo $a. " hello india <br>";
+    //      $a++;
+    //  }
+    $a=10;
+    while($a>=1){
+        echo $a. ") hello world <br>";
+        $a--;
+    }
+    ?>
+</body>
+</html>
