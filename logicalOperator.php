d814412e5a3c45975546dd04cbbd329f389caddd
developerashisroy <ashisroy40089@gmail.com>
2022-01-26 01:03:24 +0530

    comment last

---
+++ logicalOperator.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>logical operator </title>
+</head>
+<body>
+    <?php
+//  && logical AND or and both statement are true
+//  || logical OR   or or one statement always true 
+//   ! logical NOT  or xor  ture means false , false means true
+
+$age = 21;
+// if(($age>18) && ($age<25)){
+//     echo " you are eligible";
+// }
+
+// if(($age==21) && ($age<20)){
+//     echo " your are not eligible";
+// }
+
+// if (($age<20) || ($age==21)){
+//     echo " you are eligible";
+// }
+
+if($age!=20){
+    echo " true stetement";
+}
+ 
+
+
+
+
+
+    ?>
+</body>
+</html>
