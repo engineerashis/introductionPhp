ebc87e6549d18eec4da2855f8fcf5ed4674485e2
developerashisroy <ashisroy40089@gmail.com>
2022-01-25 21:49:47 +0530

    variable uses

---
+++ variable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variable </title>
+</head>
+<body>
+    <?php
+     $a="hello php <br>";
+     echo " this is php variable : ". $a;
+     echo " this is php variable : ". $a;
+     echo " this is php variable : ". $a;
+     echo " this is php variable : ". $a;
+     $b=21;
+     echo "Age :". $b;
+
+
+    ?>
+</body>
+</html>
