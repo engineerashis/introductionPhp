f679e553c99482e682049dbd6a4b6949a59cb17f
developerashisroy <ashisroy40089@gmail.com>
2022-01-26 01:53:35 +0530

    last comment

---
+++ ternary.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ternary operator</title>
+</head>
+<body>
+    <?php
+       $x=15;
+       ($x>10)? $z="Greater " : $z="Less than";
+       echo $z;
+    ?>
+</body>
+</html>
