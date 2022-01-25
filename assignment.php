a88640802934634ea515e3d9269c9f5405d7e26c
developerashisroy <ashisroy40089@gmail.com>
2022-01-25 22:45:19 +0530

    assignment operators

---
+++ assignment.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>assignment operators</title>
+</head>
+<body>
+    <?php
+    //assignment operators = , +=, -=, *=, **=,/=, %=
+    // $a=10;
+    // echo $a;
+
+    // $a=10;
+    // $a+=$a;
+    // echo $a;
+    
+    // $a=10;
+    // $a-=2;
+    // echo $a;
+    
+    // $a=10;
+    // $a*=20;
+    // echo $a;
+    
+    // $a=10;
+    // $a/=3;
+    // echo $a;
+    
+    $a=2;
+    $a**=3;
+    echo $a;
+    
+    ?>
+</body>
+</html>
