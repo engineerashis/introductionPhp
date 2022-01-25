38ea0d8e9714c2e5743706f99e5e89affa38294c
developerashisroy <ashisroy40089@gmail.com>
2022-01-26 01:45:55 +0530

    this is comment

--- switchstatement.php
+++ switchstatement.php
@@ -9,31 +9,45 @@
 <body>
     <?php
       // switch statement
-      $weekday = 4;
-      switch($weekday){
-            case 1:
-                echo "today is Monday";
-                break;
-            case 2:
-                echo "today is Tuesday";
-                break;
-            case 3:
-                echo "today is wednesday";
-                break;
-            case 4:
-                echo "today is Thursday";
-                break;
-            case 5:
-                echo "today is Friday";
-                break;
-            case 6:
-                echo "today is Saturday";
-                break;
-            case 7:
-                echo "toda is sanday";
-                break;
-            default: echo "enter valid weekday";
-        }
+    //   $weekday = 4;
+    //   switch($weekday){
+    //         case 1:
+    //             echo "today is Monday";
+    //             break;
+    //         case 2:
+    //             echo "today is Tuesday";
+    //             break;
+    //         case 3:
+    //             echo "today is wednesday";
+    //             break;
+    //         case 4:
+    //             echo "today is Thursday";
+    //             break;
+    //         case 5:
+    //             echo "today is Friday";
+    //             break;
+    //         case 6:
+    //             echo "today is Saturday";
+    //             break;
+    //         case 7:
+    //             echo "toda is sanday";
+    //             break;
+    //         default: echo "enter valid weekday";
+    //     }
+     $age=21;
+     switch($age){
+         case ($age >=15 && $age <=20):
+             echo "you are eligible";
+             break;
+         case($age>= 21 && $age <=30):
+            echo "you are not eligible";
+            break;
+        default:
+         echo "enter the valid age";
+     }
+        
+         
+     
     ?>
 </body>
 </html>
