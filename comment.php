38a68944296f149f1334f2f9e98fb1c3f93c1ea9
developerashisroy <ashisroy40089@gmail.com>
2022-01-25 22:11:05 +0530

    comment statement

---
+++ comment.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php comment</title>
+</head>
+<body>
+    <?php
+     //two type of comment
+     //single line & multiline comment
+     /**
+       this statement is multiline comment;
+      */
+
+
+
+
+     ?>
+</body>
+</html>
