7fb780d3d239c66453dff032bc64c5159aa9075e
developerashisroy <ashisroy40089@gmail.com>
2022-01-25 22:07:46 +0530

    data type

---
+++ dataType.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data type of php</title>
+</head>
+<body>
+     <?php
+     /*
+      data type of php 
+      $a="hello world" ....... string
+      $b=21;....................integer
+      $c=30.44..................float
+      $d=true/falue.............boolean
+      $e=array("HTML","CSS", "JS")..Array
+      $f=new Myclass()..........object
+      $g=null...................Null
+
+
+
+    */
+    $a="hello world"."<br>";
+    $a=21;
+    $a=30.40;
+    $a=49.646464464646464664;
+    $a=true;
+    $a=false;
+    $a=array("HTML","CSS","JS");
+    
+    echo $a[0];
+    echo "<br>";
+    var_dump($a);
+
+
+
+
+     ?>
+</body>
+</html>
