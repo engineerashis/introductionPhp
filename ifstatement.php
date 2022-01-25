11817b1c3be5901dabf8019799de4bedb43dcf14
developerashisroy <ashisroy40089@gmail.com>
2022-01-26 00:42:06 +0530

    first comment

---
+++ ifstatement.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>if statement with example</title>
+</head>
+<body>
+    <?php
+     // if statement 
+    //    $a=10;
+    //    if($a>5){
+    //        echo "this statement is true";
+    //    }
+
+    // $a=20;
+    // $b=2;
+    // if(($a<30) || ($b<5)){
+    //         echo "hello india";
+    // }
+
+    // if(true){
+    //     echo "hello world";
+    // }
+
+    if(false){
+        echo "this is false";
+    }
+    
+
+    ?>
+</body>
+</html>
