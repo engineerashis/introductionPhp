c15a972ee3e07d9d9e01bab34da605f43a9fb148
developerashisroy <ashisroy40089@gmail.com>
2022-01-26 01:12:07 +0530

    last comment

---
+++ elseIf.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>else if statement introduce</title>
+</head>
+<body>
+    <?php
+    // if else statement
+    $age=21;
+    // if($age<20){
+    //     echo " you are not eligible";               
+
+    // }else{
+    //     echo "you are eligible";
+    // }
+    if($age<30){
+        echo "$age :  is less than";
+    }else{
+        echo  "$age :  is greater than";
+    }
+
+
+
+
+
+    ?>
+</body>
+</html>
