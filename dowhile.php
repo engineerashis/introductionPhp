5bbdb2e1152f3367186bd77c293b42dc027cdad7
developerashisroy <ashisroy40089@gmail.com>
2022-01-26 02:15:30 +0530

    final comment

---
+++ dowhile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>do while loop </title>
+</head>
+<body>
+    <?php
+    //    $a=1;
+    //    do{
+    //        echo  $a. " ) hello world <br>";
+    //        $a++;
+    //    }while($a<10);
+    $a=10;
+    do{
+        echo $a. " ) hello programmer <br>";
+        $a--;
+    }while($a>=1);
+
+
+
+
+
+    ?>
+</body>
+</html>
