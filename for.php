6975283dd390a635740af4525e9fdef0d3dd39c9
developerashisroy <ashisroy40089@gmail.com>
2022-01-26 02:52:45 +0530

    last comment

---
+++ for.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>for loop </title>
+</head>
+<body>
+    <?php
+       
+    //    for($a=1; $a<=10; $a++){
+    //        echo "$a. Hello world <br>"; 
+    //    }
+    for($a=10; $a>=1; $a--){
+        echo "$a . hello world <br>";
+    }
+    ?>
+</body>
+</html>
