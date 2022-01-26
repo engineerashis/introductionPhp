f3b77ba65fb93c52dad52602b904def631a8113b
developerashisroy <ashisroy40089@gmail.com>
2022-01-27 01:28:51 +0530

    functin with parametre

--- functionwithpara.php
+++ functionwithpara.php
@@ -9,14 +9,24 @@
 <body>
     <?php
     //function with parameter;
-      function FunctionName($name1 , $name2){
-          echo "$name1 . $name2 <br>"; 
-      }
-      FunctionName("uzzal ", "roy");
-      FunctionName("sintheya"," roy");
-      FunctionName("shonjib kumar", " roy");
+    //   function FunctionName($name1 , $name2){
+    //       echo "$name1 . $name2 <br>"; 
+    //   }
+    //   FunctionName("uzzal ", "roy");
+    //   FunctionName("sintheya"," roy");
+    //   FunctionName("shonjib kumar", " roy");
     
-    
+    function FunctionName($a , $b){
+        echo $a+$b ."<br>";
+    }
+    FunctionName(10, 20);
+    for($a=1; $a<10;$a++){
+        if($a==6){
+            continue;
+        }
+        echo "$a <br>";
+    }
+    FunctionName(30,70);
     ?>
 </body>
 </html>
