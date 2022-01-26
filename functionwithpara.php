a05d55b36a9e7a9cee2f387dcc651ca468a0b5f5
developerashisroy <ashisroy40089@gmail.com>
2022-01-27 01:13:53 +0530

    function comment

---
+++ functionwithpara.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>function with parameter</title>
+</head>
+<body>
+    <?php
+      function FunctionName($name1){
+          echo "$name1 <br>";
+      }
+    
+    
+    ?>
+</body>
+</html>
