1b1d3eb6e3b6f5909f1655212ed457ddf7fb1f44
developerashisroy <ashisroy40089@gmail.com>
2022-01-27 11:12:25 +0530

    last index array

---
+++ introarray.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>arrays </title>
+</head>
+<body>
+    <?php
+    // index arrays
+
+     $cars=array("red","blue","green");
+    // $cars[0]="red";
+    // $cars[1]="blue";
+    // $cars[2]="yellow";
+    // $cars[3]="lightblue";
+    for($i=0;$i<3;$i++){
+        echo $cars[$i]."<br>";
+    }
+    //  echo $cars[0]."<br>";
+    //  echo $cars[1]."<br>";
+    //  echo $cars[2]."<br>";
+
+    // echo "<pre>";
+    // print_r($cars);//just tasting purpose;
+    // echo "<pre>";
+     ?>
+</body>
+</html>
