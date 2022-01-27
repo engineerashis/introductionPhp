a62a8a9e57a965e497204d00b9bdc0e09d7514ca
developerashisroy <ashisroy40089@gmail.com>
2022-01-27 10:09:30 +0530

    returing function

---
+++ functionWithretar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function with retarning valvo</title>
+</head>
+<body>
+    <?php 
+    // function with returing value
+
+    //    function FunctionName($fname="first", $lname="lname"){
+    //        $a="$fname $lname";
+    //        return $a;
+    //    }
+    //    $b = FunctionName("Ashis ", "Roy");
+    //    echo "hello $b";
+    // totol number of sum with parameter and returing value
+    function FunctionName($math, $phy , $che){
+        $sum=$math+$phy+$che;
+        return $sum;
+    }
+    $total=FunctionName(75, 65, 84);
+    echo $total;
+    ?>
+</body>
+</html>
